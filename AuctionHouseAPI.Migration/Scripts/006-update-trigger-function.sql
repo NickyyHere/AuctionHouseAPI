780483fb2800001641d4a291716818c79acbddac
NickyyHere <jwojcicki001@gmail.com>
2025-06-04 18:58:04 +0200

    Updated trigger for placing bid
    Now it actually works

---
+++ AuctionHouseAPI.Migration/Scripts/006-update-trigger-function.sql
@@ -0,0 +1,16 @@
+﻿CREATE OR REPLACE FUNCTION CloseAuction()
+RETURNS TRIGGER AS $$
+DECLARE
+	r_auction_options RECORD;
+BEGIN
+	SELECT * INTO r_auction_options FROM "AuctionOptions" WHERE "AuctionId" = NEW."AuctionId" FOR UPDATE;
+
+	IF r_auction_options."AllowBuyItNow" AND r_auction_options."BuyItNowPrice" <= NEW."Amount"
+	THEN
+		r_auction_options."FinishDateTime" := CURRENT_TIMESTAMP AT TIME ZONE 'UTC';
+	END IF;
+
+	UPDATE "AuctionOptions" SET "FinishDateTime" = r_auction_options."FinishDateTime" WHERE "AuctionId" = r_auction_options."AuctionId";
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
